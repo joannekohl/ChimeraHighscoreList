f450fb39ec103415534020fe9d7bf6854c0a8745
joanne thomas <joanne thomas>
2017-07-22 22:10:14 +0200

    added a dummy data json file to fill my high score list. Reading doesn't work yet

---
+++ ChimeraTest/Assets/HighscoreDataController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class HighscoreDataController : MonoBehaviour {
+
+    public string dataPath;
+
+    public DataServiceType serviceType;
+    private JsonDataService service;
+
+    // Use this for initialization
+    void Start () {
+        if (string.IsNullOrEmpty(dataPath))
+            Debug.Log("no dataPath!");
+        else
+        {
+            switch (serviceType)
+            {
+                case DataServiceType.Json:
+                    service = new JsonDataService(Path.Combine(Application.dataPath, dataPath));
+                    break;
+                case DataServiceType.Database:
+                    // service = new DatabaseDataService(Path.Combine(Application.dataPath, dataPath));
+                    break;
+                case DataServiceType.Server:
+                    // service = new ServerDataService(Path.Combine(Application.dataPath, dataPath));
+                    break;
+            }
+
+            service.Initialize();
+        }
+
+	}
+}
+
+public enum DataServiceType
+{
+    Json,
+    Database,
+    Server
+}
---
+++ ChimeraTest/Assets/JsonDataService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+public class JsonDataService {
+
+    private string jsonPath;
+
+    private List<PlayerDTO> players;
+
+    public JsonDataService(string jsonPath)
+    {
+        this.jsonPath = jsonPath;
+    }
+
+
+    public IEnumerable<PlayerDTO> GetHighscoreList()
+    {
+        return players;
+    }
+
+
+    public void Initialize()
+    {
+        // Read JSON Data and buffer it
+        string json = File.ReadAllText(jsonPath);
+        players = JsonUtility.FromJson<PlayerDTOList>(json).data.ToList();
+
+        if (!players.Any())
+            Debug.Log("oh my god!");
+        else
+            Debug.Log("player: " + JsonUtility.ToJson(players[0]));
+    }
+
+    public void Deinitialize()
+    {
+
+    }
+
+}
+
+[Serializable]
+public class PlayerDTO
+{
+    public int ID { get; set; }
+    public int Rank { get; set; }
+    public string Playername { get; set; }
+    public int Score { get; set; }
+    public string Avatar { get; set; }
+}
+
+[Serializable]
+public class PlayerDTOList
+{
+    public PlayerDTO[] data;
+}
